7a5edf37d68b90c28895104a17eefcd037827c50
Arjun Deopujari <ajd4mq@virginia.edu>
2021-01-20 03:08:02 -0500

    DALS: Change some field names in AppLoader struct

--- capsules/src/dals/app_loader.rs
+++ capsules/src/dals/app_loader.rs
@@ -26,7 +26,8 @@ pub struct AppLoader<'a> {
     verifier: &'a dyn interfaces::Verifier<'a>,
     authorizer: &'a dyn interfaces::Authorizer<'a>,
     nonvolatile_storage: &'a dyn hil::nonvolatile_storage::NonvolatileStorage<'a>,
-    app_address: Cell<usize>,
+    app_write_address: Cell<usize>,
+    app_flash: OptionalCell<&'static [u8]>,
     state: Cell<State>,
 }
 
@@ -43,21 +44,48 @@ impl<'a> AppLoader<'a> {
             verifier: verifier,
             authorizer: authorizer,
             nonvolatile_storage: nonvolatile_storage,
-            app_address: Cell::new(0),
+            nonvolatile_write_address: Cell::new(0),
             state: Cell::new(State::Idle),
         }
     }
 
-    /// TODO: Implementation should follow `app_flash` to app data in flash and check header length and version
-    pub fn check_app_header(&self) {}
-
     /// TODO: Main FSM for `DALS`
-    pub fn process_state(
-        &self,
-        buffer: Option<&'a mut [u8]>,
-        length: Option<usize>,
-        flag: Option<bool>,
-    ) {
+    pub fn process_state(&self, optional_buffer: Option<&'a mut [u8]>, optional_length: Option<usize>, optional_flag: Option<bool>) {
+
+        let buffer = optional_buffer.unwrap();
+        let length = optional_length.unwrap();
+        let flag = optional_flag.unwrap();
+
+        match self.state.get(){
+            State::DecompressBuffer => {
+                let (decomp_buf,decomp_buf_len,buffer,error) = self.decompressor.decompress_buffer(buffer,length);
+
+                let client = self.app_loader_client.take().unwrap();
+                client.return_buffer(buffer);
+                self.app_loader_client.insert(Some(client));
+
+                if flag {
+                    self.state.set(State::WriteFlashFinal);
+                } else {
+                    self.state.set(State::WriteFlash);
+                }
+                let write_address = self.app_address.get();
+                self.app_address.set(write_address + decomp_buf_len);
+                self.nonvolatile_storage.write(decomp_buf, write_address, decomp_buf_len);
+                
+            }
+            State::WriteFlash => {
+                self.decompressor.return_buffer(buffer);
+                self.state.set(State::Idle);
+            }
+            State::WriteFlashFinal => {
+                self.decompressor.return_buffer(buffer);
+                self.state.set(State::Verify);
+                self.verifier.verify_data();
+            }
+            State::
+            _ => {}
+        }
     }
 }
 
@@ -66,18 +94,19 @@ impl<'a> interfaces::AppLoader<'a> for AppLoader<'a> {
     fn start_loading(&self, app_size: usize) -> Result<(), DalsError> {
         Ok(())
     }
-    fn next_buffer(&self, data_buffer: &'a mut [u8], length: usize, completed: bool) {}
+    fn next_buffer(&self, data_buffer: &'a mut [u8], length: usize, completed: bool) {
+        self.state.set(State::DecompressBuffer);
+        self.process_state(Some(data_buffer),Some(length),Some(completed));
+    }
 }
 
 impl<'a> interfaces::VerifierClient<'a> for AppLoader<'a> {
     fn verification_complete(&self, error: Option<DalsError>) {}
 }
 
-impl<'a> interfaces::AuthorizerClient<'a> for AppLoader<'a> {
-    fn authorization_complete(&self, error: Option<DalsError>) {}
-}
-
 impl<'a> hil::nonvolatile_storage::NonvolatileStorageClient<'a> for AppLoader<'a> {
     fn read_done(&self, buffer: &'a mut [u8], length: usize) {}
-    fn write_done(&self, buffer: &'a mut [u8], length: usize) {}
+    fn write_done(&self, buffer: &'a mut [u8], length: usize) {
+        self.process_state(Some(buffer), Some(length) , None);
+    }
 }
--- capsules/src/dals/interfaces.rs
+++ capsules/src/dals/interfaces.rs
@@ -7,13 +7,12 @@ pub enum DalsError {}
 /// Implemented by any capsule (client) which can collect raw application data from an external source.
 /// Ex: UART, BLE, 802.11, etc.
 pub trait AppLoaderClient<'a> {
-    fn return_buffer(&self, data_buffer: &'static mut [u8]);
+    fn return_buffer(&self, data_buffer: &'a mut [u8]);
 }
 
 /// Implemented by main `AppLoader` capsule
 pub trait AppLoader<'a>:
     VerifierClient<'a>
-    + AuthorizerClient<'a>
     + kernel::hil::nonvolatile_storage::NonvolatileStorageClient<'a>
 {
     fn set_client(&self, client: &'a dyn AppLoaderClient);
@@ -47,11 +46,6 @@ pub trait VerifierClient<'a> {
 /// Implemented by any capsule which delivers a final decision on whether to set up process-specific structures
 /// such as stack and run the app.
 pub trait Authorizer<'a> {
-    fn set_client(&self, client: &'a dyn AuthorizerClient);
-    fn authorize_data(&self, app_flash: usize);
-}
-
-/// Implemented by main `AppLoader` module
-pub trait AuthorizerClient<'a> {
-    fn authorization_complete(&self, error: Option<DalsError>);
+    fn set_client(&self, client: &'a dyn AppLoader<'a>);
+    fn authorize_data(&self, app_flash: usize) -> Option<DalsError>;
 }
